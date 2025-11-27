06331444972ab84c726af6bc334d0f909028efc0
serii <seriiburduja@gmail.com>
2025-11-27 21:11:52 +0200

    feat: create task model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import {Task} from './tasks.model';
 
 @Injectable()
 export class TasksService {
-  private tasks: string[] = ['first', 'second', 'third'];
-  getAllTasks(): string[] {
+  private tasks: Task[] = [];
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
