5490b6bd5d14d8348aa1f4f478569640969d4db0
serii <seriiburduja@gmail.com>
2025-11-28 20:50:42 +0200

    feat: connect to typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {dataSourceOptions} from './config/data-source';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
