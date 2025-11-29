b6b5b60145c77320b2653f5e8856981578af363e
serii <seriiburduja@gmail.com>
2025-11-29 11:51:53 +0200

    feat: inject user repo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserRepository } from './user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, UserRepository],
 })
 export class AuthModule {}
